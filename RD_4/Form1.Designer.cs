61f52fa6eea1a801266303920542c7471234135a
zdTang <44347399+zdTang@users.noreply.github.com>
2018-12-02 07:14:15 -0500

    Grid to display customer

--- RD_4/Form1.Designer.cs
+++ RD_4/Form1.Designer.cs
@@ -28,13 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Grid = new System.Windows.Forms.DataGridView();
+            this.btnDisplayCustomer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Grid
+            // 
+            this.Grid.AllowUserToAddRows = false;
+            this.Grid.AllowUserToDeleteRows = false;
+            this.Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid.Location = new System.Drawing.Point(46, 21);
+            this.Grid.Name = "Grid";
+            this.Grid.ReadOnly = true;
+            this.Grid.Size = new System.Drawing.Size(442, 136);
+            this.Grid.TabIndex = 0;
+            // 
+            // btnDisplayCustomer
+            // 
+            this.btnDisplayCustomer.Location = new System.Drawing.Point(69, 177);
+            this.btnDisplayCustomer.Name = "btnDisplayCustomer";
+            this.btnDisplayCustomer.Size = new System.Drawing.Size(182, 23);
+            this.btnDisplayCustomer.TabIndex = 1;
+            this.btnDisplayCustomer.Text = "Display Customers";
+            this.btnDisplayCustomer.UseVisualStyleBackColor = true;
+            this.btnDisplayCustomer.Click += new System.EventHandler(this.btnDisplayCustomer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDisplayCustomer);
+            this.Controls.Add(this.Grid);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView Grid;
+        private System.Windows.Forms.Button btnDisplayCustomer;
     }
 }
 
--- RD_4/Form1.cs
+++ RD_4/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,28 @@ namespace RD_4
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnDisplayCustomer_Click(object sender, EventArgs e)
+        {
+            string sql = "select * from customer";
+            using (MySqlConnection conn = new MySqlConnection("datasource = 127.0.0.1; database = ztwally; port = 3306; username = root;password=tl6902856; sslmode = none"))
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                conn.Open();
+                MySqlDataReader reader = cmd.ExecuteReader();
+                Grid.Rows.Clear();
+                Grid.Columns.Clear();
+                Grid.Columns.Add("customerID", "Customer ID");
+                Grid.Columns.Add("first", "First Name");
+                Grid.Columns.Add("last", "Last Name");
+                Grid.Columns.Add("phone", "Phone Number");
+                while (reader.Read())
+                {
+                    Grid.Rows.Add(reader["customerID"], reader["firstName"], reader["lastName"], reader["phoneNumber"]);
+                }
+                reader.Close();
+            }
         }
     }
 }
